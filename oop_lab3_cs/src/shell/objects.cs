2019b87292f170ef225ea952ab25859820445068
Yaroslav Protsenko <yarik.pro.ph@gmail.com>
2017-02-13 03:02:34 +0200

    Objects API rewritten in C#

---
+++ oop_lab3_cs/src/shell/objects.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace oop_lab3_cs.shell.objects {
+
+    public class TypeError : Exception { }
+
+    public abstract class ShObject {
+
+        public static ShObject New<T>(T value) {
+            return new ShObjectImpl<T>(value);
+        }
+
+        public static ShObject Empty() {
+            return new EmptyShObject();
+        }
+
+        public bool HasType<T>() {
+            if (typeof(T) == typeof(void)) return this is EmptyShObject;
+            return this is ShObjectImpl<T>;
+        }
+
+        public bool IsEmpty { get { return this is EmptyShObject;  } }
+
+        public abstract bool HasSameType(ShObject other);
+
+        public T Get<T>() {
+            if (!HasType<T>()) throw new TypeError();
+            return ((ShObjectImpl<T>)this).GetData();
+        }
+
+        public abstract override string ToString();
+
+    }
+
+
+    class ShObjectImpl<T> : ShObject {
+
+        T data;
+
+        public ShObjectImpl(T data) { this.data = data; }
+
+        public override string ToString() {
+            return data.ToString();
+        }
+
+        public T GetData() { return data; }
+
+        public override bool HasSameType(ShObject other) {
+            return other is ShObjectImpl<T>;
+        }
+
+    }
+
+    class EmptyShObject: ShObject {
+
+        public override bool HasSameType(ShObject other) {
+            return other is EmptyShObject;
+        }
+
+        public override string ToString() {
+            return "";
+        }
+
+    }
+
+}
---
+++ oop_lab3_cs_tests/test_shell_objects.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using oop_lab3_cs.shell.objects;
+
+
+namespace oop_lab3_cs_tests {
+
+    [TestClass]
+    public class DynamicTypesTests {
+
+        [TestMethod]
+        public void TestApiWorks() {
+            var obj = ShObject.New<int>(10);
+
+            Assert.IsTrue(obj.HasSameType(obj));
+            Assert.IsTrue(obj.HasType<int>());
+            Assert.IsFalse(obj.HasType<string>());
+            Assert.IsFalse(obj.IsEmpty);
+            Assert.AreEqual(obj.Get<int>(), 10);
+
+            try { obj.Get<float>();
+            } catch (TypeError) { return; }
+            Assert.Fail("Get<wrong-type>() must throw TypeError");
+        }
+
+        [TestMethod]
+        public void TestSupportsEmptyValue() {
+            var empty_obj = ShObject.Empty();
+            Assert.IsTrue(empty_obj.IsEmpty);
+
+            try { empty_obj.Get<float>();
+            } catch (TypeError) { return; }
+            Assert.Fail("Get<any-type>() on Empties must throw TypeError");
+        }
+
+        [TestMethod]
+        public void TestTwoObjectsWithSameType() {
+            var obj1 = ShObject.New<string>("example1");
+            var obj2 = ShObject.New<string>("example2");
+            Assert.IsTrue(obj1.HasSameType(obj2));
+            Assert.IsTrue(obj2.HasSameType(obj1));
+        }
+
+        [TestMethod]
+        public void TestTwoObjectsWithDifferentTypes() {
+            var obj1 = ShObject.New<string>("example1");
+            var obj2 = ShObject.New<float>(1.0f);
+            Assert.IsFalse(obj1.HasSameType(obj2));
+            Assert.IsFalse(obj2.HasSameType(obj1));
+        }
+
+};
+
+}
