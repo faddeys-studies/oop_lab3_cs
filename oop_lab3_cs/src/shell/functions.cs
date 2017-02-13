a982010aa7a140140f7a2fc78f048737d33f0aeb
Yaroslav Protsenko <yarik.pro.ph@gmail.com>
2017-02-13 05:28:03 +0200

    Functions are implemented in C# (not rewritten because of high use of templates in C++ version)

---
+++ oop_lab3_cs/src/shell/functions.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+
+using oop_lab3_cs.shell.objects;
+
+
+namespace oop_lab3_cs.shell.functions {
+
+    [AttributeUsage(AttributeTargets.Method)]
+    public class ExposeToShell : Attribute {
+        public readonly string HelpText;
+
+        public ExposeToShell(string text) { this.HelpText = text; }
+    }
+
+
+    public class ShFunction {
+
+        private readonly MethodInfo method;
+        private readonly string[] arg_names;
+        private readonly Type[] arg_types;
+        private readonly Type return_type;
+
+        public ShFunction(Type holder, string method_name) 
+            : this(holder.GetMethod(method_name)) { }
+
+        public ShFunction(MethodInfo method) {
+            if (!method.IsStatic) throw new ArgumentException("Only static methods are allowed");
+            this.method = method;
+            this.return_type = method.ReturnType;
+            this.arg_types = (from param in method.GetParameters()
+                              select param.ParameterType).ToArray();
+            this.arg_names = (from param in method.GetParameters()
+                              select param.Name).ToArray();
+        }
+
+        public ShObject Call(Dictionary<string, ShObject> arguments) {
+            foreach (var arg_name in arg_names) {
+                if (!arguments.ContainsKey(arg_name))
+                    throw new ShellError("Missing argument: " + arg_name);
+            }
+            if (arguments.Count > arg_names.Length)
+                throw new ShellError("Unexpected arguments");
+            var args = Enumerable.Zip(
+                arg_names, arg_types,
+                (arg_name, arg_type) => arguments[arg_name].Get(arg_type)
+            ).ToArray();
+            object result = method.Invoke(null, args);
+            if (return_type == typeof(void)) {
+                return ShObject.Empty();
+            } else {
+                return ShObject.New(return_type, result);
+            }
+        }
+
+    }
+
+}
---
+++ oop_lab3_cs_tests/test_shell_functions.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using oop_lab3_cs.shell.objects;
+using oop_lab3_cs.shell.functions;
+
+
+namespace oop_lab3_cs_tests {
+
+    using Args = Dictionary<string, ShObject>;
+
+    class Dummy {
+
+        public int x;
+        public string str;
+
+        public Dummy(int x, string str) {
+            this.x = x;
+            this.str = str;
+        }
+    }
+
+    class Dummy2 {
+
+        public int x;
+
+        public Dummy2(int x) {
+            this.x = x;
+        }
+    }
+
+    class DummyApiHolder {
+        public static void MyMutate(Dummy arg1) {
+            arg1.x += 1;
+        }
+        public static Dummy MyNoArgs() {
+            return new Dummy(10, "some string");
+        }
+        public static Dummy MyCombine(Dummy arg1, Dummy arg2) {
+            return new Dummy(arg1.x + arg2.x, arg1.str + arg2.str);
+        }
+        public static Dummy MyTwoTypes(Dummy arg1, Dummy2 arg2) {
+            return new Dummy(arg1.x * arg2.x, arg1.str);
+        }
+    }
+
+    [TestClass]
+    public class ShellExtensionApiTests {
+        [TestMethod]
+        public void TestFunctionWithoutReturn() {
+            ShFunction mutate = new ShFunction(typeof(DummyApiHolder), "MyMutate");
+            ShObject dummy_obj = ShObject.New<Dummy>(new Dummy(100, "xyz"));
+
+            ShObject result = mutate.Call(new Args {
+                { "arg1", dummy_obj}
+            });
+
+            Assert.IsTrue(result.IsEmpty);
+            Assert.AreEqual(dummy_obj.Get<Dummy>().x, 101);
+        }
+
+        [TestMethod]
+        public void TestFunctionWithoutArguments() {
+            ShFunction no_args = new ShFunction(typeof(DummyApiHolder), "MyNoArgs");
+
+            ShObject result = no_args.Call(new Args {});
+
+            Assert.IsTrue(result.HasType<Dummy>());
+            Assert.AreEqual(result.Get<Dummy>().x, 10);
+            Assert.AreEqual(result.Get<Dummy>().str, "some string");
+        }
+
+        [TestMethod]
+        public void TestFunctionWithArgsAndReturn() {
+            ShFunction combine = new ShFunction(typeof(DummyApiHolder), "MyCombine");
+            ShObject dummy1_obj = ShObject.New<Dummy>(new Dummy(100, "xyz"));
+            ShObject dummy2_obj = ShObject.New<Dummy>(new Dummy(250, "asd"));
+
+            ShObject result = combine.Call(new Args {
+                { "arg1", dummy1_obj},
+                { "arg2", dummy2_obj}
+            });
+
+            Assert.IsTrue(result.HasType<Dummy>());
+            Assert.AreEqual(result.Get<Dummy>().x, 350);
+            Assert.AreEqual(result.Get<Dummy>().str, "xyzasd");
+        }
+
+        [TestMethod]
+        public void TestFunctionWithDifferentParameterTypes() {
+            ShFunction two_types = new ShFunction(typeof(DummyApiHolder), "MyTwoTypes");
+            ShObject dummy1_obj = ShObject.New<Dummy>(new Dummy(100, "xyz"));
+            ShObject dummy2_obj = ShObject.New<Dummy2>(new Dummy2(25));
+
+            ShObject result = two_types.Call(new Args {
+                { "arg1", dummy1_obj},
+                { "arg2", dummy2_obj}
+            });
+
+            Assert.IsTrue(result.HasType<Dummy>());
+            Assert.AreEqual(result.Get<Dummy>().x, 2500);
+            Assert.AreEqual(result.Get<Dummy>().str, "xyz");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ShellError))]
+        public void TestMissingParameterRaisesTypeError() {
+            ShFunction combine = new ShFunction(typeof(DummyApiHolder), "MyCombine");
+            ShObject dummy1_obj = ShObject.New<Dummy>(new Dummy(100, "xyz"));
+            ShObject dummy2_obj = ShObject.New<Dummy>(new Dummy(250, "asd"));
+
+            combine.Call(new Args {
+                { "arg1", dummy1_obj}
+            });
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ShellError))]
+        public void TestWrongParameterNameRaisesTypeError() {
+            ShFunction combine = new ShFunction(typeof(DummyApiHolder), "MyCombine");
+            ShObject dummy1_obj = ShObject.New<Dummy>(new Dummy(100, "xyz"));
+            ShObject dummy2_obj = ShObject.New<Dummy>(new Dummy(250, "asd"));
+
+            combine.Call(new Args {
+                { "arg1",  dummy1_obj},
+                { "wrong", dummy1_obj}
+            });
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ShellError))]
+        public void TestExtraParameterRaisesTypeError() {
+            ShFunction combine = new ShFunction(typeof(DummyApiHolder), "MyCombine");
+            ShObject dummy1_obj = ShObject.New<Dummy>(new Dummy(100, "xyz"));
+            ShObject dummy2_obj = ShObject.New<Dummy>(new Dummy(250, "asd"));
+
+            combine.Call(new Args {
+                { "arg1", dummy1_obj},
+                { "arg2", dummy2_obj},
+                { "extra", dummy1_obj}
+            });
+        }
+
+    }
+}
